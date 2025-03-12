eaa4311e2219d55d21e6e711a1b5f3d9bed1a46d
Alexander <minlex.ais@gmail.com>
2025-03-12 15:44:06 +0300

    feat: updated service tests

--- src/test/java/com/store/service/ProductServiceTest.java
+++ src/test/java/com/store/service/ProductServiceTest.java
@@ -1,108 +1,110 @@
-//package com.store.service;
-//
-//import com.store.OnlineStoreApplication;
-//import com.store.dto.ProductDto;
-//import com.store.entity.Product;
-//import com.store.repository.ProductRepository;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.PageImpl;
-//import org.springframework.test.context.bean.override.mockito.MockitoBean;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//@SpringBootTest(classes = OnlineStoreApplication.class)
-//public class ProductServiceTest {
-//
-//    @MockitoBean
-//    private ProductRepository productRepository;
-//
-//    @Autowired
-//    private ProductService productService;
-//
-//    @Test
-//    void getAllProductsSortingByName() {
-//        Product product = new Product(10L, "product", "desc", 12.34, "url", 15, null);
-//        when(productRepository.findByNameContainsIgnoreCaseOrderByNameAsc(any(), any())).thenReturn(
-//                new PageImpl<>(List.of(product))
-//        );
-//
-//        Page<ProductDto> page = productService.findAll(0, 10, "", "name");
-//        Assertions.assertEquals(1, page.getTotalElements());
-//
-//        ProductDto productDto = page.getContent().getFirst();
-//        Assertions.assertEquals(product.getId(), productDto.id());
-//        Assertions.assertEquals(product.getName(), productDto.name());
-//        Assertions.assertEquals(product.getDescription(), productDto.description());
-//        Assertions.assertEquals(product.getPrice(), productDto.price());
-//        Assertions.assertEquals(product.getImageUrl(), productDto.imageUrl());
-//        Assertions.assertEquals(product.getStock(), productDto.stock());
-//    }
-//
-//    @Test
-//    void getAllProductsSortingByPrice() {
-//        Product product = new Product(10L, "product", "desc", 12.34, "url", 15, null);
-//        when(productRepository.findByNameContainsIgnoreCaseOrderByPriceAsc(any(), any())).thenReturn(
-//                new PageImpl<>(List.of(product))
-//        );
-//
-//        Page<ProductDto> page = productService.findAll(0, 10, "", "price");
-//        Assertions.assertEquals(1, page.getTotalElements());
-//
-//        ProductDto productDto = page.getContent().getFirst();
-//        Assertions.assertEquals(product.getId(), productDto.id());
-//        Assertions.assertEquals(product.getName(), productDto.name());
-//        Assertions.assertEquals(product.getDescription(), productDto.description());
-//        Assertions.assertEquals(product.getPrice(), productDto.price());
-//        Assertions.assertEquals(product.getImageUrl(), productDto.imageUrl());
-//        Assertions.assertEquals(product.getStock(), productDto.stock());
-//    }
-//
-//    @Test
-//    void findProductById() {
-//        Product product = new Product(10L, "product", "desc", 10.12, "url", 3, null);
-//
-//        when(productRepository.findById(any())).thenReturn(Optional.of(product));
-//
-//        ProductDto productDto = productService.findById(10L);
-//
-//        Assertions.assertNotNull(productDto);
-//        Assertions.assertEquals(product.getId(), productDto.id());
-//        Assertions.assertEquals(product.getName(), productDto.name());
-//        Assertions.assertEquals(product.getDescription(), productDto.description());
-//        Assertions.assertEquals(product.getPrice(), productDto.price());
-//        Assertions.assertEquals(product.getImageUrl(), productDto.imageUrl());
-//        Assertions.assertEquals(product.getStock(), productDto.stock());
-//        Assertions.assertEquals(product.getCartQuantity(), productDto.cartQuantity());
-//
-//        verify(productRepository).findById(10L);
-//    }
-//
-//    @Test
-//    void findProductNull() {
-//        when(productRepository.findById(any())).thenReturn(Optional.empty());
-//
-//        ProductDto productDto = productService.findById(10L);
-//
-//        Assertions.assertNull(productDto);
-//    }
-//
-//    @Test
-//    void saveProduct() {
-//        when(productRepository.save(any())).thenReturn(null);
-//
-//        ProductDto productDto = new ProductDto(100L, "product1", "desc", 10.56, "url", 8, 0);
-//
-//        productService.save(productDto);
-//
-//        verify(productRepository).save(any(Product.class));
-//    }
-//}
+package com.store.service;
+
+import com.store.OnlineStoreApplication;
+import com.store.dto.PageResponse;
+import com.store.dto.ProductDto;
+import com.store.entity.Product;
+import com.store.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = OnlineStoreApplication.class)
+public class ProductServiceTest {
+
+    @MockitoBean
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void getAllProductsSortingByName() {
+        Product product = new Product(10L, "product", "desc", 12.34, "url", 15, 0);
+
+        when(productRepository.findByNameContainingOrderByName(any(), anyInt(), anyInt())).thenReturn(Flux.just(product));
+        when(productRepository.countByNameContaining(any())).thenReturn(Mono.just(7L));
+
+        Mono<PageResponse<ProductDto>> result = productService.searchProducts(0, 10, "", "name");
+
+        StepVerifier.create(result)
+                .expectNextMatches(
+                        el -> el.getTotalElements() == 7
+                                && el.getData().size() == 1
+                                && el.getData().getFirst().id().equals(product.getId())
+                                && el.getData().getFirst().name().equals(product.getName())
+                                && el.getData().getFirst().price().equals(product.getPrice())
+                )
+                .expectComplete()
+                .verify();
+
+        verify(productRepository).findByNameContainingOrderByName("%%", 10, 0);
+        verify(productRepository).countByNameContaining("%%");
+    }
+
+    @Test
+    void getAllProductsSortingByPrice() {
+        Product product = new Product(10L, "product", "desc", 12.34, "url", 15, 0);
+
+        when(productRepository.findByNameContainingOrderByPrice(any(), anyInt(), anyInt())).thenReturn(Flux.just(product));
+        when(productRepository.countByNameContaining(any())).thenReturn(Mono.just(7L));
+
+        Mono<PageResponse<ProductDto>> result = productService.searchProducts(0, 10, "", "price");
+
+        StepVerifier.create(result)
+                .expectNextMatches(
+                        el -> el.getTotalElements() == 7
+                                && el.getData().size() == 1
+                                && el.getData().getFirst().id().equals(product.getId())
+                                && el.getData().getFirst().name().equals(product.getName())
+                                && el.getData().getFirst().price().equals(product.getPrice())
+                )
+                .expectComplete()
+                .verify();
+
+        verify(productRepository).findByNameContainingOrderByPrice("%%", 10, 0);
+        verify(productRepository).countByNameContaining("%%");
+    }
+
+    @Test
+    void findProductById() {
+        Product product = new Product(10L, "product", "desc", 10.12, "url", 3, 0);
+
+        when(productRepository.findById(anyLong())).thenReturn(Mono.just(product));
+
+        Mono<ProductDto> result = productService.findById(10L);
+
+        StepVerifier.create(result)
+                .expectNextMatches(
+                        el -> el.id().equals(product.getId())
+                                && el.name().equals(product.getName())
+                                && el.price().equals(product.getPrice())
+                )
+                .expectComplete()
+                .verify();
+
+        verify(productRepository).findById(10L);
+    }
+
+    @Test
+    void saveProduct() {
+        when(productRepository.save(anyString(), anyString(), anyDouble(), anyString(), anyInt())).thenReturn(Mono.just(new Product()));
+
+        ProductDto productDto = new ProductDto(100L, "product1", "desc", 10.56, "url", 8, 0);
+
+        Mono<Void> result = productService.save(productDto);
+
+        StepVerifier.create(result)
+                .verifyComplete();
+
+        verify(productRepository).save("product1", "desc", 10.56d, "url", 8);
+    }
+}
